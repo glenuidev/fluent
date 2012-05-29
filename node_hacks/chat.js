e24b491acab99b8544ac1a65d5ee21aada3e51a2
Glen C <gcuthber@cisco.com>
2012-05-29 16:34:01 -0700

    updated code

--- node_hacks/chat.js
+++ node_hacks/chat.js
@@ -1,30 +1,31 @@
 // basic chat server
 // usage: telnet localhost 8001
-var net = require( 'net' );
-var server = net.createServer();
+var net, server;
+net = require( 'net' );
+server = net.createServer();
 
-server.listen(8001);
+server.listen( 8001 );
 
 // represents all connections
 var connections = [];
 
 
 server.on( 'connection', function connection ( socket ) {
-	socket.write('hello');
+	socket.write( 'hello! welcome to the chat server' );
 	console.log( 'New connection' );
 	// give them a way to get to multiple connections
 	connections.push(socket);
-
+	
 	socket.on( 'data', function ( data ) {
-		for (var i=0;i<connections.length;i++){
-			if(connections[i].writeable){
-				connections[i].write(data);
+		for ( var i=0; i < connections.length; i++ ){
+			if( connections[i].writeable ){
+				connections[i].write( data );
 			}
 		}
-		console.log(data);
+		console.log( data.toString() );
 	});
-
+	
 	socket.on( 'close', function (){
-		connections.splice(connections.indexOf(socket),1);
+		connections.splice( connections.indexOf( socket ), 1 );
 	})
 });
